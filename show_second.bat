116c06ec9654224b422e3081c3075e458b8452aa
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-03-25 22:04:43 +0300

    Update show_second.bat

--- show_second.bat
+++ show_second.bat
@@ -1,1 +1,15 @@
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSecondsInSystemClock /t REG_DWORD /d 1 /f
+@echo off
+echo [1] Show Clock Seconds
+echo [2] Hide Clock Seconds
+
+CHOICE /C 12 /M "Please make a choice:"
+
+IF %ERRORLEVEL%==1 (
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSecondsInSystemClock /t REG_DWORD /d 1 /f
+    echo Clock seconds are now visible.
+) ELSE IF %ERRORLEVEL%==2 (
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSecondsInSystemClock /t REG_DWORD /d 0 /f
+    echo Clock seconds are now hidden.
+)
+
+pause
